664de8102235688a21a3970085a28b0b632d2646
maash3r <maamimsh988@gmail.com>
2020-12-04 19:33:13 +0530

    Fixed Adapter Class

--- app/src/main/java/com/example/adblocker_project/ui/dashboard/MyListAdapter.java
+++ app/src/main/java/com/example/adblocker_project/ui/dashboard/MyListAdapter.java
@@ -9,6 +9,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.adblocker_project.R;
@@ -16,12 +17,22 @@ import com.example.adblocker_project.R;
 
 public class MyListAdapter extends RecyclerView.Adapter<MyListAdapter.ViewHolder>{
 
+    @NonNull
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
         View listItem= layoutInflater.inflate(R.layout.list_item, parent, false);
-        ViewHolder viewHolder = new ViewHolder(listItem);
-        return viewHolder;
+        return new ViewHolder(listItem);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Use holder to update the data here
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0; // Change this to return the count of elements to display
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
